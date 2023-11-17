1b3a11698bdfa697ea4c3ddcd591c107a6dc4331
James Cahill <jcahill773@gmail.com>
2023-11-17 11:56:30 +0000

    (backend): = instead of == for postgres

--- backend/util/user.go
+++ backend/util/user.go
@@ -11,7 +11,8 @@ import (
 func IsUuidFree[T any](db *gorm.DB, id uuid.UUID, obj *T) bool {
 	// Run a SQL query to see if the user exists
 	var user structs.User
-	err := db.Model(obj).Select("id").Where("id == ?", id.String()).First(&user).Error
+	// == works in SQLite, not in Postgres
+	err := db.Model(obj).Select("id").Where("id = ?", id.String()).First(&user).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		// Record not found, so user id is free
 		return true
