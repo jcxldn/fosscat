63bbafacdefb4383c94e58c17776e699ce7e930e
James Cahill <jcahill773@gmail.com>
2023-11-21 11:06:46 +0000

    (backend/util): Rename user.go, remove legacy user struct

--- backend/util/uuid.go
+++ backend/util/uuid.go
@@ -4,15 +4,12 @@ import (
 	"errors"
 
 	"github.com/google/uuid"
-	"github.com/jcxldn/fosscat/backend/structs"
 	"gorm.io/gorm"
 )
 
 func IsUuidFree[T any](db *gorm.DB, id uuid.UUID, obj *T) bool {
-	// Run a SQL query to see if the user exists
-	var user structs.User
-	// == works in SQLite, not in Postgres
-	err := db.Model(obj).Select("id").Where("id = ?", id.String()).First(&user).Error
+	// NOTE: == works in SQLite, not in Postgres
+	err := db.Model(obj).Select("id").Where("id = ?", id.String()).First(&obj).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		// Record not found, so user id is free
 		return true
