35f99f03b6c7797cc305b3a29426ec42bdffe026
James Cahill <jcahill773@gmail.com>
2023-12-15 12:27:21 +0000

    Add initial tests to prove that everything works

--- backend/util/get_test.go
+++ backend/util/get_test.go
@@ -1,19 +1,44 @@
 package util_test
 
 import (
-	"fmt"
 	"testing"
 
+	"github.com/jcxldn/fosscat/backend/database"
+	"github.com/jcxldn/fosscat/backend/graph/model"
+	"github.com/jcxldn/fosscat/backend/structs"
 	"github.com/jcxldn/fosscat/backend/test/common"
+	"github.com/jcxldn/fosscat/backend/util"
 	"github.com/stretchr/testify/suite"
 )
 
 type UtilGetTestSuite struct {
 	common.DatabaseTestSuite
+	user *structs.User
+}
+
+func (s *UtilGetTestSuite) TestCreateUser() {
+	newUser := model.NewUser{FirstName: "Example", LastName: "User", Email: "example.user@example.com"}
+	user, err := database.CreateUser(s.DB, newUser)
+
+	if err != nil {
+		panic(err)
+	}
+
+	s.user = user
 }
 
 func (s *UtilGetTestSuite) TestGetObjectByIdUser() {
-	fmt.Println("uwu")
+	user, err := util.GetObjectById[structs.User](s.DB, s.user.ID)
+
+	if err != nil {
+		panic(err)
+	}
+
+	s.Assertions.Equal(s.user.ID, user.ID)
+}
+
+func (s *UtilGetTestSuite) TestUuidIsFreeUser() {
+	s.Assertions.False(util.IsUuidFree[structs.User](s.DB, s.user.ID))
 }
 
 // In order for 'go test' to run this suite, we need to create
