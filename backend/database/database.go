77addcf18acdce9af27ea5d1ce4dcea01698b3d5
James Cahill <jcahill773@gmail.com>
2023-12-15 12:08:31 +0000

    (backend/database/migrate): Add preliminary error handling

--- backend/database/database.go
+++ backend/database/database.go
@@ -34,17 +34,25 @@ func Connect() *gorm.DB {
 func Migrate(db *gorm.DB) {
 	log.Println("[database] migrating...")
 
+	var errors [4]error
+
 	// "Migrate" the schema
 	// This will create tables, keys, columns, etc. Everything really.
 	// See https://gorm.io/docs/migration.html
 	// Note that we need to pass each struct in our schema.
 
-	db.AutoMigrate(&structs.Checkout{})
+	errors[0] = db.AutoMigrate(&structs.Checkout{})
 	// Item has a dependency on Entity, so do them in the correct order
 	// to avoid "relation does not exist" error during table creation.
-	db.AutoMigrate(&structs.Entity{})
-	db.AutoMigrate(&structs.Item{})
-	db.AutoMigrate(&structs.User{})
+	errors[1] = db.AutoMigrate(&structs.Entity{})
+	errors[2] = db.AutoMigrate(&structs.Item{})
+	errors[3] = db.AutoMigrate(&structs.User{})
+
+	for index, err := range errors {
+		if err != nil {
+			log.Panicf("Error with migrate call %d: '%s'", index, err)
+		}
+	}
 
 	log.Println("[database] migrated, done.")
 }
