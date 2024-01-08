f8e855f24bd66188de233a18277baf4dddbcf7ad
James Cahill <jcahill773@gmail.com>
2024-01-08 15:39:49 +0000

    (backend/util/jwt): Add claims struct, return time.time for getExpiryTime, add VerifyJwt command

--- backend/util/jwt/jwt.go
+++ backend/util/jwt/jwt.go
@@ -7,21 +7,43 @@ import (
 	"github.com/jcxldn/fosscat/backend/structs"
 )
 
-func getExpiryTime() int64 {
+type Claims struct {
+	jwt.RegisteredClaims
+}
+
+func getExpiryTime() time.Time {
 	time := time.Now()
 	time = time.AddDate(0, 0, 1) // one day
-	return time.Unix()
+	return time
 }
 
 func NewJwt(user structs.User) (str string, err error) {
-	claims := jwt.MapClaims{
-		"iss": "fosscat",
-		"sub": user.ID,
-		"exp": getExpiryTime(),
+	claims := Claims{
+		jwt.RegisteredClaims{
+			Issuer:    "fosscat",
+			Subject:   user.ID.String(),
+			ExpiresAt: jwt.NewNumericDate(getExpiryTime()),
+		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodES384, claims)
 
 	str, err = token.SignedString(key)
 
 	return
+}
+
+func VerifyJwt(jwtStr string, user structs.User) (token *jwt.Token, claims *Claims, err error) {
+	token, err = jwt.ParseWithClaims(jwtStr, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return &key.PublicKey, nil
+	})
+
+	if token == nil {
+		return nil, nil, err
+	}
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return token, claims, nil
+	} else {
+		return nil, nil, err
+	}
 }
