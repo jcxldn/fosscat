bae34b90c8566b92af7995d422a2b92be46708ab
James Cahill <jcahill773@gmail.com>
2024-01-16 15:50:50 +0000

    (frontend): PoC for expo-barcode-scanner using docs
    https://docs.expo.dev/versions/latest/sdk/bar-code-scanner/

--- frontend/app/(app)/scan.tsx
+++ frontend/app/(app)/scan.tsx
@@ -1,11 +1,52 @@
 import React from "react";
+import { Text, View } from "../../components/Themed";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { Button, StyleSheet } from "react-native";
 
 const ScanPage = () => {
+    // States
+    const [hasPermission, setHasPermission] = React.useState<Boolean | null>(null);
+    const [scanned, setScanned] = React.useState(false);
+
+    React.useEffect(() => {
+        console.log("DEFINE GBSP")
+        const getBarcodeScannerPermissions = async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === "granted")
+            console.log(`Status: ${status}`)
+        };
+
+        getBarcodeScannerPermissions();
+    });
+
+    const handleScan = ({ type, data }) => {
+        setScanned(true)
+        alert(`Barcode scanned with type ${type} and data ${data}.`)
+    }
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+
+    if (!hasPermission) {
+        return <Text>Camera permission denied.</Text>
+    }
+
     return (
-        <>
-            Hello, world!
-        </>
+        <View style={StyleSheet.absoluteFillObject}>
+            <Text>hi</Text>
+            <BarCodeScanner style={StyleSheet.absoluteFillObject} onBarCodeScanned={scanned ? undefined : handleScan}>
+                {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+            </BarCodeScanner>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        height: "100%",
+        width: "100%"
+    }
+});
 
 export default ScanPage;
