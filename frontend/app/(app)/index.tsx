9bb7a109cb62b36a7cc42f68b062c20e2dd6292b
James Cahill <jcahill773@gmail.com>
2024-01-16 15:50:09 +0000

    (frontend/index): Surround text in <text>

--- frontend/app/(app)/index.tsx
+++ frontend/app/(app)/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Text } from "../../components/Themed";
 
 const HomePage = () => {
     return (
         <>
-            Hello, world!
+            <Text>Hello, world!</Text>
         </>
     )
 }
