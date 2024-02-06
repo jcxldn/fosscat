7a0e493615e3a02411f2e5698007531e5ab616d4
James Cahill <jcahill773@gmail.com>
2024-02-06 13:13:42 +0000

    (backend/util/filetype): Seek back to start of file after reading first 512 bytes
    This means that the whole file is read on the next read() call, allowing it to be saved properly.

--- backend/util/filetype.go
+++ backend/util/filetype.go
@@ -12,7 +12,16 @@ var imageFileTypes = []string{"image/jpeg", "image/png"}
 
 func GetFileTypeForFile(file graphql.Upload) (structs.FileTypes, error) {
 	buf := make([]byte, 512) // only need first 512 bytes to determine file type per docs
-	if _, err := file.File.Read(buf); err == nil {
+
+	// Read the first 512 bytes of the file
+	_, err := file.File.Read(buf)
+
+	// Seek back to the beginning of the file
+	// so that future reads (such as saving in the DB) contain the whole file
+	// Inspired to a similar problem at: https://stackoverflow.com/a/61729136
+	file.File.Seek(0, 0)
+
+	if err == nil {
 		// Read file successfully
 		contentType := http.DetectContentType(buf)
 		if slices.Contains(imageFileTypes, contentType) {
