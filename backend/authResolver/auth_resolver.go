927da515949c7ce5ff518b8255915adac76d5415
James Cahill <jcahill773@gmail.com>
2024-04-01 13:51:47 +0100

    (backend/authResolver): add comments

--- backend/authResolver/auth_resolver.go
+++ backend/authResolver/auth_resolver.go
@@ -23,8 +23,12 @@ func QueryResolver[T any](ctx context.Context, resolver func(user *structs.User)
 		// Type assertion that "user" ctx is not nil and is of type structs.User
 		userStruct := user.(*structs.User)
 
+		// Call the resolver function passed as a param
 		returnData := resolver(userStruct)
+
+		// Get the return date from the ReturnFactory
 		data, err := returnData()
+		// Return the data, casting to T where necessary.
 		return data.(T), err
 	} else {
 		var null T
