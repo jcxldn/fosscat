af764596a3128c52c3f785aa4662eb7f7badc78c
James Cahill <jcahill773@gmail.com>
2024-04-09 15:50:10 +0100

    (frontend/util): Improved error handling for isJwtExpired

--- frontend/util/jwt.ts
+++ frontend/util/jwt.ts
@@ -2,16 +2,23 @@ import { jwtDecode } from "jwt-decode";
 
 const isJwtExpired = (jwtStr: (string | null)): boolean => {
     if (jwtStr) {
-        // Parse token
-        const decoded = jwtDecode(jwtStr);
+        try {
+            // Parse token
+            const decoded = jwtDecode(jwtStr);
 
-        // Parse expiry time, falling back to epoch 0
-        // x1000 to convert to ms (epoch)
-        const expiry = new Date(0)
-        expiry.setUTCSeconds(decoded.exp || 0)
+            // Parse expiry time, falling back to epoch 0
+            // x1000 to convert to ms (epoch)
+            const expiry = new Date(0)
+            expiry.setUTCSeconds(decoded.exp || 0)
 
-        return Date.now() > expiry.getTime()
+            return Date.now() > expiry.getTime()
+        } catch (error) {
+            // Error decoding, display the error and return true for expired.
+            console.error(error)
+            return true
+        }
     } else {
+        console.warn("[isJwtExpired] jwt not specified.")
         // no token specified, return true for expired.
         return true
     }
